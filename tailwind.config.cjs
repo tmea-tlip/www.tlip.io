67ae560e2e320584a405b4aad8e633890cf85136
evavirseda <evirseda@boxfish.studio>
2022-06-09 18:51:17 +0200

    Add color palette and custom styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,3 +1,5 @@
+const pxToRem = (px, base = 16) => `${px / base}rem`
+
 module.exports = {
     purge: {
         content: [
@@ -15,7 +17,52 @@ module.exports = {
                 '2xl': '7rem',
             },
         },
-        extend: {}
+        extend: {
+            colors: {
+                blue: {
+                    1: '#006EA9',
+                    2: '#2D9CDB',
+                    3: '#56CCF2'
+                },
+                green: {
+                    1: '#002E00',
+                    2: '#219653',
+                    3: '#6FCF97'
+                },
+                grey: {
+                    1: '#333333',
+                    2: '#4F4F4F',
+                    3: '#BDBDBD'
+                },
+            },
+            fontSize: {
+                13: pxToRem(13),
+                14: pxToRem(14),
+                15: pxToRem(15),
+                16: pxToRem(16),
+                18: pxToRem(18),
+                20: pxToRem(20),
+                22: pxToRem(22),
+                24: pxToRem(24),
+                32: pxToRem(32),
+                40: pxToRem(40),
+                48: pxToRem(48),
+                54: pxToRem(54),
+                56: pxToRem(56),
+                64: pxToRem(64),
+            },
+            lineHeight: {
+                100: '100%',
+                110: '110%',
+                120: '120%',
+                130: '130%',
+                140: '140%',
+                160: '160%',
+            },
+            letterSpacing: {
+                0.02: '0.02em',
+            },
+        }
     },
     variants: {
         extend: {
