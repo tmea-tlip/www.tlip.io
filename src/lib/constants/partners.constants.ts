193504d461c27bacf812e1fde7cf25190fdc4247
evavirseda <evirseda@boxfish.studio>
2022-10-05 15:32:06 +0200

    add hm government logo

--- src/lib/constants/partners.constants.ts
+++ src/lib/constants/partners.constants.ts
@@ -1,57 +1,58 @@
 const GOVERNMENT_PARTNERS = {
-  /**
-   *
-   */
-  title: "Our Donors and Government Partners",
-  /**
-   *
-   */
-  logos: [
-    "/assets/partners/AFA-logo.png",
-    "/assets/partners/KRA-logo.png",
-    "/assets/partners/KEPHIS-logo.png",
-    "/assets/partners/KENTrade-logo.png",
-    "/assets/partners/Trademark-logo.png",
-    "/assets/partners/ministry-foreign-affairs-netherlands-logo.png"
-  ]
+    /**
+     *
+     */
+    title: "Our Donors and Government Partners",
+    /**
+     *
+     */
+    logos: [
+        "/assets/partners/AFA-logo.png",
+        "/assets/partners/KRA-logo.png",
+        "/assets/partners/KEPHIS-logo.png",
+        "/assets/partners/KENTrade-logo.png",
+        "/assets/partners/Trademark-logo.png",
+        "/assets/partners/ministry-foreign-affairs-netherlands-logo.png",
+        "/assets/partners/hm-government-logo.png"
+    ]
 };
 
 const COMMERCIAL_PARTNERS = {
-  /**
-   *
-   */
-  title: "Our Commercial Partners",
-  /**
-   *
-   */
-  logos: [
-    "/assets/partners/institute-of-export-logo.png",
-    "/assets/partners/premier-nutrition-logo.png",
-    "/assets/partners/geodis-logo.png",
-    "/assets/partners/royal-flora-holland-logo.png",
-    "/assets/partners/CNSI-logo.png",
-    "/assets/partners/MCP-logo.png",
-    "/assets/partners/mansys-logo.png",
-    "/assets/partners/SCT-logo.png",
-    "/assets/partners/coriolis-logo.png",
-    "/assets/partners/TAPA-logo.png",
-    "/assets/partners/DP-world-logo.png",
-    "/assets/partners/university-surrey-logo.png",
-    "/assets/partners/DFDS-logo.png",
-    "/assets/partners/beyond-now-logo.png",
-    "/assets/partners/kenya-airways-logo.png",
-    "/assets/partners/british-coffee-association-logo.png"
-    // "/assets/partners/morning-foods-logo.png",
-    // "/assets/partners/AWS-logo.png",
-    // "/assets/partners/british-coffee-association-logo.png",
-    // "/assets/partners/nomad-foods-logo.png",
-    // "/assets/partners/BT-logo.png",
-    // "/assets/partners/FPC-logo.png",
-    // "/assets/partners/RAS-logo.png",
-    // "/assets/partners/forum-future-logo.png",
-    // "/assets/partners/OLAM-logo.png",
-    // "/assets/partners/meatco-logo.png"
-  ]
+    /**
+     *
+     */
+    title: "Our Commercial Partners",
+    /**
+     *
+     */
+    logos: [
+        "/assets/partners/institute-of-export-logo.png",
+        "/assets/partners/premier-nutrition-logo.png",
+        "/assets/partners/geodis-logo.png",
+        "/assets/partners/royal-flora-holland-logo.png",
+        "/assets/partners/CNSI-logo.png",
+        "/assets/partners/MCP-logo.png",
+        "/assets/partners/mansys-logo.png",
+        "/assets/partners/SCT-logo.png",
+        "/assets/partners/coriolis-logo.png",
+        "/assets/partners/TAPA-logo.png",
+        "/assets/partners/DP-world-logo.png",
+        "/assets/partners/university-surrey-logo.png",
+        "/assets/partners/DFDS-logo.png",
+        "/assets/partners/beyond-now-logo.png",
+        "/assets/partners/kenya-airways-logo.png",
+        "/assets/partners/british-coffee-association-logo.png"
+        // "/assets/partners/morning-foods-logo.png",
+        // "/assets/partners/AWS-logo.png",
+        // "/assets/partners/british-coffee-association-logo.png",
+        // "/assets/partners/nomad-foods-logo.png",
+        // "/assets/partners/BT-logo.png",
+        // "/assets/partners/FPC-logo.png",
+        // "/assets/partners/RAS-logo.png",
+        // "/assets/partners/forum-future-logo.png",
+        // "/assets/partners/OLAM-logo.png",
+        // "/assets/partners/meatco-logo.png"
+    ]
 };
 
 export const PARTNERS = [GOVERNMENT_PARTNERS, COMMERCIAL_PARTNERS];
