b7a1ac592bcfa9584f305380b07c54765fe44073
evavirseda <evirseda@boxfish.studio>
2022-10-07 10:03:33 +0200

    fix error

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,14 +1,12 @@
 <script lang="ts">
-    import { Button } from '$components'
-    import { page } from '$app/stores';
-    export let status
-    export let error
+    import { Button } from "$components";
+    import { page } from "$app/stores";
 
     const BUTTON = {
-        title: 'Home',
-        as: 'link',
-        url: '/',
-    }
+        title: "Home",
+        as: "link",
+        url: "/"
+    };
 </script>
 
 <svelte:head>
@@ -19,12 +17,12 @@
     <div class="container flex flex-col items-center">
         <div class="flex flex-col items-center text-center justify-center z-10 mb-5 text-grey-500">
             <h2 class="uppercase flex text-64 leading-none">
-                {status}
+                {$page.status}
             </h2>
-            <h4 class="my-2">{$page.error}</h4>
+            <h4 class="my-2">{$page.error.message}</h4>
         </div>
         <div class="flex home-button">
-            <Button {...BUTTON} small/>
+            <Button {...BUTTON} small />
         </div>
     </div>
 </section>
