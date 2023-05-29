ef54f8fdff70e0e060535cf99e2d8990960d20ea
Jean Carlo Noguera <jcnoguera@boxfish.studio>
2023-05-29 11:32:28 +0200

    fix: remove alias from vite config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
 import { sveltekit } from "@sveltejs/kit/vite";
-import path from "path";
 
 /** @type {import('vite').UserConfig} */
 const config = {
-    plugins: [sveltekit()],
-    resolve: {
-        alias: {
-            $components: `${path.resolve(__dirname, "src/components")}/`,
-            $sections: `${path.resolve(__dirname, "src/sections")}/`
-        }
-    }
+    plugins: [sveltekit()]
 };
 
 export default config;
