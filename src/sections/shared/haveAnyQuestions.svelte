a08196ad34a3d11dd9f184927f7822c201856224
Begoña Álvarez de la Cruz <balvarez@boxfish.studio>
2023-12-01 12:20:13 +0100

    feat: add LinkedIn icon "Any Questions?" section (#24)
    * feat: add LinkedIn icon "Any Questions?" section
    
    * chore: add more space for buttons
    
    ---------
    
    Co-authored-by: JCNoguera <jcnoguera@boxfish.studio>

--- src/sections/shared/haveAnyQuestions.svelte
+++ src/sections/shared/haveAnyQuestions.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
     import type { Button as ButtonType } from "$lib/types/components";
-    import { Button } from "$components";
+    import { Button, Icon } from "$components";
     import type { Heading } from "$lib/types/sections";
     import { page } from "$app/stores";
+    import type { ComponentProps } from "svelte";
 
     export let id: string;
 
@@ -25,13 +26,28 @@
         title: "Still Have Questions?",
         description: "Send us an email and we will reply as soon as we can."
     };
+
+    const LINKEDIN_SOCIAL: {
+        href: string;
+        icon: ComponentProps<Icon>;
+    } = {
+        href: "https://www.linkedin.com/company/trade-and-logistics-information-pipeline/",
+        icon: {
+            name: "linkedin",
+            fill: "#000",
+            height: 24,
+            width: 24
+        }
+    };
 </script>
 
 <section class="py-20 bg-blue-300" {id}>
     <div class="container flex text-white justify-between flex-col text-center lg:flex-row lg:text-left">
-        <div class="w-full lg:mr-7">
+        <div class="py-20 w-full lg:mr-7">
             {#if $page.url.pathname != "/faq"}
-                <h1 class="capitalize mb-6 flex text-36 md:text-48 justify-center lg:justify-start">{HEADING.title}</h1>
+                <h1 class="capitalize mb-6 flex text-36 md:text-48 justify-center lg:justify-start">
+                    {HEADING.title}
+                </h1>
                 <p>{HEADING.description}</p>
             {:else}
                 <h1 class="capitalize mb-6 flex text-36 md:text-48 justify-center lg:justify-start">
@@ -39,6 +55,7 @@
                 </h1>
                 <p>{HEADING_FAQS.description}</p>
             {/if}
+
             <div class="buttons w-full md:flex gap-x-7 mt-6 md:mt-10 lg:mt-28">
                 {#if $page.url.pathname != "/faq"}
                     {#each BUTTONS as button}
@@ -46,6 +63,14 @@
                             <Button classes="text-16 font-bold h-14" {...button} secondary />
                         </div>
                     {/each}
+                    <a
+                        href={LINKEDIN_SOCIAL.href}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        class="w-1/2 md:w-auto flex p-4 bg-white rounded-md mx-auto justify-center mb-6 last:mb-0 md:mb-0 hover:opacity-80 transition-opacity"
+                    >
+                        <Icon {...LINKEDIN_SOCIAL.icon} />
+                    </a>
                 {:else}
                     <div class="w-1/2 uppercase mx-auto lg:mx-0 mb-6 last:mb-0 md:mb-0">
                         <Button classes="text-16 font-bold h-14" {...BUTTONS[1]} secondary />
@@ -53,6 +78,7 @@
                 {/if}
             </div>
         </div>
+
         <div class="w-full lg:max-w-md flex relative object-scale-down items-center mt-12 lg:mt-0 justify-center">
             <img src="./assets/haveAnyQuestions.png" alt="TLIP in Africa" />
         </div>
@@ -62,7 +88,7 @@
 <style lang="scss">
     .buttons {
         @screen lg {
-            max-width: 412px;
+            max-width: 468px;
         }
     }
 </style>
