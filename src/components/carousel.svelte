8e3e46ed4891c86f29f3383c0cca1bdaafad66e7
Martyn Janes <martyn@obany.com>
2024-02-15 09:30:06 +0200

    Revert swiper version

--- src/components/carousel.svelte
+++ src/components/carousel.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-	// eslint-disable-next-line import/no-unresolved
-	import { register } from "swiper/element/bundle";
-	// eslint-disable-next-line import/no-unresolved
-	import { Autoplay, FreeMode } from "swiper/modules";
+	import { Autoplay, FreeMode } from "swiper";
+	import { Swiper, SwiperSlide } from "swiper/svelte";
+	// Import Swiper styles
 	// eslint-disable-next-line import/no-unresolved
 	import "swiper/css";
 
-	export let images: string[];
+	export let images: string[] = [];
+	export let id: string;
 
 	let innerWidth: number;
 
@@ -15,63 +15,58 @@
 	const SLIDE_GUTTER = 40; // px
 	const MAX_CAROUSEL_WIDTH = 1600; // px
 
-	let sliderWidth = 0;
-
+	let swiperOptions: { [key: string]: unknown } = {};
 	$: {
-		const imageLength = SLIDE_WIDTH * images?.length;
-		const gutterLength = SLIDE_GUTTER * (images?.length - 1);
+		const imageLength = images ? SLIDE_WIDTH * images?.length : 0;
+		const gutterLength = images ? SLIDE_GUTTER * (images?.length - 1) : 0;
 
-		sliderWidth = imageLength + gutterLength;
+		const sliderWidth = imageLength + gutterLength;
+		const staticCarousel = sliderWidth <= Math.min(innerWidth ?? MAX_CAROUSEL_WIDTH, MAX_CAROUSEL_WIDTH);
+
+		swiperOptions = {
+			slidesPerView: "auto",
+			class: "mySwiper",
+			spaceBetween: 40,
+			centeredSlides: !staticCarousel,
+			centerInsufficientSlides: true,
+			modules: [Autoplay, FreeMode],
+			freeMode: {
+				enabled: !staticCarousel,
+				sticky: false,
+				momentumBounce: true,
+				momentumRatio: 0.4,
+				momentumVelocityRatio: 0.4,
+				minimumVelocity: 0
+			},
+			preventClicks: true,
+			loop: !staticCarousel,
+			slideToClickedSlide: true,
+			speed: staticCarousel ? 0 : 2000,
+			autoplay: {
+				enabled: !staticCarousel,
+				delay: 0,
+				disableOnInteraction: false
+			}
+		};
 	}
-
-	$: staticCarousel = sliderWidth <= Math.min(innerWidth, MAX_CAROUSEL_WIDTH);
-
-	$: swiperOptions = {
-		centeredSlides: !staticCarousel,
-		modules: [Autoplay, FreeMode],
-		freeMode: {
-			enabled: !staticCarousel,
-			sticky: false,
-			momentumBounce: true,
-			momentumRatio: 0.4,
-			momentumVelocityRatio: 0.4,
-			minimumVelocity: 0
-		},
-		loop: !staticCarousel,
-		speed: staticCarousel ? 0 : 2000,
-		autoplay: {
-			enabled: !staticCarousel,
-			delay: 0,
-			disableOnInteraction: false
-		}
-	};
-
-	register();
 </script>
 
 <svelte:window bind:innerWidth />
 
-<div class="swiper-block" style="--max-width: {MAX_CAROUSEL_WIDTH}px;">
+<div class="swiper-block" style="--max-width: {MAX_CAROUSEL_WIDTH}px;" {id}>
 	{#key swiperOptions}
-		<swiper-container
-			slidesPerView="auto"
-			spaceBetween={SLIDE_GUTTER}
-			centerInsufficientSlides
-			preventClicks
-			slideToClickedSlide
-			{...swiperOptions}
-		>
+		<Swiper {...swiperOptions}>
 			{#each images as image}
-				<swiper-slide style={`width: ${SLIDE_WIDTH}px; height: ${SLIDE_HEIGHT}px;`}>
+				<SwiperSlide>
 					<div
 						class="rounded border border-grey-100 p-4"
 						style={`width: ${SLIDE_WIDTH}px; height: ${SLIDE_HEIGHT}px;`}
 					>
 						<img src={image} alt="" class="h-full w-full object-contain" />
 					</div>
-				</swiper-slide>
+				</SwiperSlide>
 			{/each}
-		</swiper-container>
+		</Swiper>
 	{/key}
 </div>
 
