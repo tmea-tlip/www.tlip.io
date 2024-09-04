4c60f46beba00af6c789b23b62b01129440e7135
evavirseda <evirseda@boxfish.studio>
2024-09-04 12:27:10 +0200

    feat: add standards page (#45)
    * feat: add standards page
    
    * fix format
    
    * feat: add missing content

---
+++ src/components/info.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+</script>
+
+<div class="flex flex-col gap-y-5 [&_a]:text-green-500 [&_a]:underline">
+	<div class="leading-125 tracking-0.02">
+		<slot name="title" />
+	</div>
+	<div class="leading-6 tracking-0.04">
+		<slot name="description" />
+	</div>
+	<div class="[&_ul]:list-disc [&_ul]:pl-5">
+		<slot name="items" />
+	</div>
+	<div class="leading-6 tracking-0.04">
+		<slot name="conclusion" />
+	</div>
+</div>
---
+++ src/routes/standard-compliance/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { ObservedRoute } from "$components";
+	import type { Section } from "$lib";
+	import { HaveAnyQuestions } from "$sections/shared";
+	import { Standards, Hero } from "$sections/standard-compliance";
+
+	const sections: Section[] = [
+		{
+			component: Hero,
+			id: "hero"
+		},
+		{
+			component: Standards,
+			id: "standards"
+		},
+		{
+			component: HaveAnyQuestions,
+			id: "HaveAnyQuestions"
+		}
+	];
+</script>
+
+<ObservedRoute {sections} />
