8bd1c7c76e1abfe7460ba60a183fd702c41bfe81
evavirseda <evirseda@boxfish.studio>
2024-09-04 10:50:10 +0200

    feat: add standards page

---
+++ src/routes/standard-compliance/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { ObservedRoute } from "$components";
+	import type { Section } from "$lib";
+	import { Standards, Hero } from "$sections/standard-compliance";
+	import { HaveAnyQuestions } from "$sections/shared";
+
+	const sections: Section[] = [
+		{
+			component: Hero,
+			id: "hero"
+		},
+		{
+			component: Standards,
+			id: "standards"
+		},
+		{
+			component: HaveAnyQuestions,
+			id: "HaveAnyQuestions"
+		}
+	];
+</script>
+
+<ObservedRoute {sections} />
---
+++ src/sections/standard-compliance/hero.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { Hero } from "$components";
+	import type { Heading } from "$lib";
+
+	const heading: Heading = {
+		title: "Standards Compliance"
+	};
+</script>
+
+<Hero {heading} backgroundOpacity="opacity-50" />
