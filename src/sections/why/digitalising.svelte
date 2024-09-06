8a0b7e42f98cf906121ef1a7abee2296fb31f254
evavirseda <evirseda@boxfish.studio>
2024-09-06 09:03:16 +0200

    feat: update colors

--- src/sections/why/digitalising.svelte
+++ src/sections/why/digitalising.svelte
@@ -29,8 +29,8 @@
 	<h2 class="text-center">Digitalising trade transactions would…</h2>
 	<div class="flex flex-1 flex-col gap-8 lg:flex-row lg:gap-5">
 		{#each items as { title, icon }}
-			<div class="flex flex-col items-center gap-5 rounded-lg border border-blue-500 p-10 text-center lg:w-1/3">
-				<Icon {...icon} fill="#004E71" width={50} height={50} />
+			<div class="flex flex-col items-center gap-5 rounded-lg border border-grey-100 p-10 text-center lg:w-1/3">
+				<Icon {...icon} fill="#000" width={50} height={50} />
 				<h4>{title}</h4>
 			</div>
 		{/each}
